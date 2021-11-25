2ae4b72baa0583609d48acf89b411625d5586310
Andres_Acosta <andresdavidacostasarmiento@gmail.com>
2021-11-25 02:37:10 -0500

    commit inicial

---
+++ src/app/services/clientes.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientesService {
+
+  url = "http://127.0.0.1:3000/";
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAllClientes() {
+    return this.http.get(`${this.url}getAllcliente`);
+  }
+
+  Insertar(cliente: any): Observable<any> {
+    return this.http.post(`${this.url}addCLiente`, cliente);
+  }
+
+  Actualizar(cliente: any, documento: number) {
+    return this.http.put(`${this.url}/updateCliente/` + documento, cliente);
+  }
+
+  Eliminar(documento: number) {
+    return this.http.delete(`${this.url}deleteCliente/${documento}`);
+  } 
+}
